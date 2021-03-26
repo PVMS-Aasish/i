cd9d0e292c628914f739b5e61bf8b035d7950e58
PVMS-Aasish <remo.p77@gmail.com>
2021-03-26 14:54:46 +0530

    changes done

---
+++ components/CustomSideBarMenu.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet,TouchableOpacity } from 'react-native';
+import {DrawerItems} from 'react-navigation-drawer'
+import db from 'firebase'
+
+export default class CustomSideBarMenu extends Component{
+  render(){
+    return(
+      <View >  <Text>custom side bar menu</Text>         </View>
+    )  } }
