8635649e83d6932b5cb8436ef563b173210f0039
PVMS-Aasish <remo.p77@gmail.com>
2021-03-29 14:55:23 +0530

    changes done

--- components/CustomSideBarMenu.js
+++ components/CustomSideBarMenu.js
@@ -4,7 +4,53 @@ import {DrawerItems} from 'react-navigation-drawer'
 import db from 'firebase'
 
 export default class CustomSideBarMenu extends Component{
-  render(){
-    return(
-      <View >  <Text>custom side bar menu</Text>         </View>
-    )  } }
+    render(){
+      return(
+        <View style={styles.container}>  
+        <View style={styles.drawerItemsContainer}> 
+        <DrawerItems {...this.props}/>
+        </View>
+     
+        <View style={styles.logOutContainer}>
+<TouchableOpacity style={styles.logOutButton}
+  onPress={()=>{
+    this.props.navigation.navigate('WelcomeScreen')
+    firebase.auth().signOut()
+  }}>
+  <Text style={styles.logOutText}>LOG OUT</Text>
+</TouchableOpacity>
+
+        </View>
+       </View>
+                
+               
+      )  } }
+
+      const styles = StyleSheet.create({
+        container:{
+          flex:1,
+                 
+        },
+        drawerItemsContainer:{
+          flex:0.8,
+                 
+        },
+        logOutContainer : {
+               flex:0.2, 
+                  justifyContent:'flex-end',
+                      paddingBottom:30  
+              },
+        logOutButton : {
+                  height:30,
+                  width:'100%',    
+                  justifyContent:'center',    
+                  padding:10  
+            },
+        logOutText:{
+              fontSize: 30,    
+              fontWeight:'bold'  
+            }
+      
+      })
+
+  
